3060b6fb2b9a9cb8205bfcb61fd917c5bf4f757a
Estangler <estanglerv@gmail.com>
2024-05-19 19:31:08 -0300

    js revew

---
+++ index.js
@@ -0,0 +1,100 @@
+
+
+/*
+1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert('Hello World!')
+------------------------------------------------------------------------------------------------------------------------
+
+2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+let numOne = 5
+let numTwo = 5
+document.write(numOne + numTwo)
+------------------------------------------------------------------------------------------------------------------------
+3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    
+    💡 Para saber o tipo de dado você pode usar o operador `typeof`
+
+let isNumb = 10
+
+if(isNaN(isNumb)) {
+    alert('não é um número')
+}else {
+    alert('é um número')
+}
+
+console.log(typeof(isNumb))
+
+------------------------------------------------------------------------------------------------------------------------
+    
+4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let isNumb = 10
+
+if(isNaN(isNumb)) {
+    alert('não é um número')
+}else {
+    alert('é um número')
+}
+
+console.log(typeof(isNumb))
+
+------------------------------------------------------------------------------------------------------------------------
+
+5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+let isBoolean = true
+
+isBoolean = 10
+
+if(typeof isBoolean === 'boolean') {
+    alert('é um boolean')
+}else {
+    alert('Não é um boolean')
+}
+
+------------------------------------------------------------------------------------------------------------------------
+6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+
+let a = 10
+let b = 20
+
+alert(`a subtração entre ${b} - ${a} é :` + (b - a))
+------------------------------------------------------------------------------------------------------------------------
+7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+let a = 2
+let b = 5
+
+alert('a multiplicação entre as duas váriaveis é :' + a*b)
+------------------------------------------------------------------------------------------------------------------------
+8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+let a = 25
+let b = 5
+
+alert('A divisão é: ' + a/b )
+------------------------------------------------------------------------------------------------------------------------
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso 
+contrário, exiba a mensagem "Não é um número par".
+
+let a = 11;
+
+if(a%2 == 0 ) {
+    alert('É par')
+} else (
+    alert('Não é par')
+)
+------------------------------------------------------------------------------------------------------------------------
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+let a = 11;
+
+if(a%2 == 0 ) {
+    alert('É par')
+} else (
+    alert('Não é par')
+)
+------------------------------------------------------------------------------------------------------------------------
+*/
+
